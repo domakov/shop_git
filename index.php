bfe7c2b49eb0fa6dd68d1bd4734b6ac8c742b3c8
klimsalamat <klim.salamat.98@mail.ru>
2017-05-31 11:08:31 +0500

    php

--- index.php
+++ index.php
@@ -32,72 +32,64 @@
   <!-- end of header --> 
  
 <?
-$sql="SELECT * from tovar";
+$sql="SELECT * from tovar 
+INNER JOIN foto ON tovar.id_tovar = foto.id_foto";
 	$res=fetchAll($sql);
 ?>
   <!-- ********************** --> 
   <!--     I N T R O          --> 
   <!-- ********************** --> 
-  <?var_dump($res);?>
+	<script type="text/javascript" src="js/jquery/jquery.slides.js"></script> 
+    <script type="text/javascript" src="js/shoppica.products_slide.js"></script>
   <div id="intro">
- +    <div id="intro_wrap">
- +      <div id="product_intro" class="container_12">
- +        <div id="product_intro_info" class="grid_5">
-          <div style="position: relative;">
- +            <h2><a href="product.html">Samsung SyncMaster 941BW</a></h2>
- +            <div class="s_rating_holder">
- +              <p class="s_rating s_rating_big s_rating_5"> <span style="width: 60%;" class="s_percent"></span> </p>
- +              <span class="s_average">3 out of 5</span> </div>
- +            <p class="s_desc"> Imagine the advantages of going big without slowing down. The big 19&quot; 941BW monitor combines wide aspect ratio with fast pixel response time, for bigger images, more room to work and crisp motion. In addition, the exclusive MagicBright 2, MagicColor and MagicTune technologies help deliver th...</p>
- +            <div class="s_price_holder">
- +              <p class="s_price"> <span class="s_currency s_before">$</span>384.67 </p>
- +            </div>
- +          </div>
- +          <div style="position: relative; display: none;">
- +            <h2><a href="product.html">White football trainers</a></h2>
- +            <p class="s_desc"> Product 8 ...</p>
- +            <div class="s_price_holder">
- +              <p class="s_price"> <span class="s_currency s_before">$</span>192.34 </p>
- +            </div>
- +          </div>
- +          <div style="position: relative; display: none;">
- +            <h2><a href="product.html">Balenciaga Paris</a></h2>
- +            <p class="s_desc"> Intel Core 2 Duo processor Powered by an Intel Core 2 Duo processor at speeds up to 2.16GHz, the new MacBook is the fastest ever 1GB memory, larger hard drives The new MacBook now comes with 1GB of memory standard and larger hard drives for the entire line perfect for runni...</p>
- +            <div class="s_price_holder">
- +              <p class="s_price"> <span class="s_currency s_before">$</span>961.68 </p>
- +            </div>
- +          </div>
- +          <div style="position: relative; display: none;">
- +            <h2><a href="product.html">Adidas Acqua</a></h2>
- +            <p class="s_desc"> Product 8 Morbi malesuada, ante sit amet faucibus pharetra, ipsum ante imperdiet tellus, nec placerat metus sem sed magna. Quisque id urna a metus porttitor tristique et at justo. Praesent non enim in elit sagittis feugiat. Integer in lectus lobortis lacus commodo mattis non auctor metus. Sus...</p>
- +            <div class="s_price_holder">
- +              <p class="s_price"> <span class="s_currency s_before">$</span>192.34 </p>
- +            </div>
- +          </div>
- +          <div style="position: relative; display: none;">
- +            <h2><a href="product.html">Panasonic Lumix</a></h2>
- +            <p class="s_desc"> Engineered with pro-level features and performance, the 12.3-effective-megapixel D300 combines brand new technologies with advanced features inherited from Nikon&#39;s newly announced D3 professional digital SLR camera to offer serious photographers remarkable performance combined with agilit...</p>
- +            <div class="s_price_holder">
- +              <p class="s_price"> <span class="s_currency s_before">$</span>153.87 </p>
- +            </div>
- +          </div>
- +        </div>
- +        <div id="product_intro_preview">
- +          <div class="slides_container">
- +            <div class="slideItem" style="display: none"><a href="product.html"><img src="images/dummy/pic_1.jpg" alt="" /></a></div>
- +            <div class="slideItem" style="display: none"><a href="product.html"><img src="images/dummy/pic_2.jpg" alt=""  /></a></div>
- +            <div class="slideItem" style="display: none"><a href="product.html"><img src="images/dummy/pic_3.jpg" alt="" /></a></div>
- +            <div class="slideItem" style="display: none"><a href="product.html"><img src="images/dummy/pic_4.jpg" alt="" /></a></div>
- +            <div class="slideItem" style="display: none"><a href="product.html"><img src="images/dummy/pic_5.jpg" alt="" /></a></div>
- +          </div>
- +          <a class="s_button_prev" href="javascript:;"></a>
- +          <a class="s_button_next" href="javascript:;"></a>
- +        </div>
- +      </div>
- +    </div>
- +  </div>
- +	<script type="text/javascript" src="js/jquery/jquery.slides.js"></script> 
- +  <script type="text/javascript" src="js/shoppica.products_slide.js"></script>
+      <div id="intro_wrap">
+        <div id="product_intro" class="container_12">
+          <div id="product_intro_info" class="grid_5">
+		  <?
+		  $i=0;
+		  foreach($res as $array){
+		  $i++;
+		  ?>
+				<div style="position: <?if ($i==1){
+				echo "relative;";} 
+				else 
+				{
+				echo"none;";}
+				?>">
+				  <h2><a href="product.html"><?=$array["t_name"];?></a></h2>
+				  <div class="s_rating_holder">
+					<p class="s_rating s_rating_big s_rating_5"> <span style="width: 60%;" class="s_percent"></span> </p>
+					<span class="s_average">3 out of 5</span> </div>
+				  <p class="s_desc"> 
+				  <?=$array["t_description"];
+				  ?>
+				  </p>
+				  <div class="s_price_holder">
+					<p class="s_price"> <span class="s_currency s_before">$</span>384.67 </p>
+				  </div>
+				</div>
+				<?}?>
+          </div>
+          <div id="product_intro_preview">
+            <div class="slides_container">
+			<?
+		  
+		  foreach($res as $array){
+		
+		  ?>
+              <div class="slideItem" style="display: none"><a href="product.php"><img src="<?=$array["f_link"];?>" alt="" /></a></div>
+              
+			  <?
+			  }
+			  ?>
+            </div>
+            <a class="s_button_prev" href="javascript:;"></a>
+            <a class="s_button_next" href="javascript:;"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  
   <!-- end of intro --> 
   
   
@@ -116,6 +108,7 @@ $sql="SELECT * from tovar";
 
 </h2>
       <div class="clear"></div>
+	  
       <div class="s_listing s_grid_view clearfix">
         <div class="s_item grid_2"> <a class="s_thumb" href="product.php"><img src="images/dummy/pic_5.jpg" title="Armani Acqua di Gioia" alt="Armani Acqua di Gioia" /></a>
           <h3><a href="product.php">Armani Acqua di Gioia</a></h3>
@@ -123,37 +116,7 @@ $sql="SELECT * from tovar";
           <p class="s_price s_promo_price"><span class="s_old_price"><span class="s_currency s_before">$</span>1,538.69</span><span class="s_currency s_before">$</span>1,346.35</p>
           <a class="s_button_add_to_cart" href="product.php"><span class="s_icon_16"><span class="s_icon"></span>Add to Cart</span></a>
         </div>
-        <div class="s_item grid_2"> <a class="s_thumb" href="product.php"><img src="images/dummy/pic_6.jpg" title="Hennesy Paradis Extra" alt="Hennesy Paradis Extra" /></a>
-          <h3><a href="product.php">Hennesy Paradis Extra</a></h3>
-          <p class="s_model">Product 19</p>
-          <p class="s_price s_promo_price"><span class="s_old_price"><span class="s_currency s_before">$</span>1,923.36</span><span class="s_currency s_before">$</span>1,731.02</p>
-          <a class="s_button_add_to_cart" href="product.php"><span class="s_icon_16"><span class="s_icon"></span>Add to Cart</span></a>
-        </div>
-        <div class="s_item grid_2"> <a class="s_thumb" href="product.php"><img src="images/dummy/pic_7.jpg" title="Leica M7" alt="Leica M7" /></a>
-          <h3><a href="product.php">Leica M7</a></h3>
-          <p class="s_model">Product 4</p>
-          <p class="s_price s_promo_price"><span class="s_old_price"><span class="s_currency s_before">$</span>1,538.69</span><span class="s_currency s_before">$</span>1,346.35</p>
-          <a class="s_button_add_to_cart" href="product.php"><span class="s_icon_16"><span class="s_icon"></span>Add to Cart</span></a>
-        </div>
-        <div class="s_item grid_2"> <a class="s_thumb" href="product.php"><img src="images/dummy/pic_8.jpg" title="Sony VAIO" alt="Sony VAIO" /></a>
-          <h3><a href="product.php">Sony VAIO</a></h3>
-          <p class="s_model">Product 9</p>
-          <p class="s_price s_promo_price"><span class="s_old_price"><span class="s_currency s_before">$</span>899.99</span><span class="s_currency s_before">$</span>1,731.02</p>
-          <p class="s_rating s_rating_5"><span style="width: 100%;" class="s_percent"></span></p>
-          <a class="s_button_add_to_cart" href="product.php"><span class="s_icon_16"><span class="s_icon"></span>Add to Cart</span></a>
-        </div>
-        <div class="s_item grid_2"> <a class="s_thumb" href="product.php"><img src="images/dummy/pic_9.jpg" title="Sony Cyber Shot S1000" alt="Sony Cyber Shot S1000" /></a>
-          <h3><a href="product.php">Sony Cyber Shot S1000</a></h3>
-          <p class="s_model">Product 8</p>
-          <p class="s_price s_promo_price"><span class="s_old_price"><span class="s_currency s_before">$</span>99,999.99</span><span class="s_currency s_before">$</span>9,999.99</p>
-          <a class="s_button_add_to_cart" href="product.php"><span class="s_icon_16"><span class="s_icon"></span>Add to Cart</span></a>
-        </div>
-        <div class="s_item grid_2"> <a class="s_thumb" href="product.php"><img src="images/dummy/pic_10.jpg" title="Panasonic Lumix" alt="Panasonic Lumix" /></a>
-          <h3><a href="product.php">Panasonic Lumix</a></h3>
-          <p class="s_model">Product 4</p>
-          <p class="s_price s_promo_price"><span class="s_old_price"><span class="s_currency s_before">$</span>1,923.36</span><span class="s_currency s_before">$</span>500.00</p>
-          <a class="s_button_add_to_cart" href="product.php"><span class="s_icon_16"><span class="s_icon"></span>Add to Cart</span></a>
-        </div>
+        
         <div class="clear"></div>
       </div>
     </div>
