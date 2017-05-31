bda08d4770d04e2a1c3b9bcd2a7c4adee90b1ca6
Nurzhaugan <kuatzhanova13.07@mail.ru>
2017-05-31 10:09:21 +0300

    12

---
+++ includes/lis_right_col.php
@@ -0,0 +1,168 @@
+  <div id="right_col" class="grid_3">
+      <div id="cart_side" class="s_box_1 s_cart_holder">
+        <h2 class="s_secondary_color">Шоппинг арбашасы</h2>
+        <div id="cart_side_contents">
+          <p class="s_mb_0">0 элементтер</p>
+        </div>
+      </div>
+      
+      <div id="categories_module" class="s_box">
+        <h2>Санаттар</h2>
+        <div class="s_list_1">
+          <ul>
+          	<li>
+            	<a href="listing_1.html"><b>Электроника</b></a>
+            	<ul>
+              	<li><a href="listing_1.html">сандық фотоаппарат</a></li>
+                <li><a href="listing_1.html">Басты аудио</a></li>
+                <li><a href="listing_1.html">Үй кинотеатры</a></li>
+                <li><a href="listing_1.html">Ұялы телефондар</a></li>
+                <li><a href="listing_1.html">MP3 Ойыншылар</a></li>
+                <li><a href="listing_1.html">Аудио машина</a></li>
+              </ul>
+            </li>
+            <li><a href="listing_1.html">Компьютерлер</a></li>
+            <li><a href="listing_1.html">киім</a></li>
+            <li><a href="listing_1.html">Аяқ киім</a></li>
+            <li><a href="listing_1.html">Сыйлықтар</a></li>
+            <li><a href="listing_1.html">Үй үшін</a></li>
+            <li><a href="listing_1.html">Денсаулық</a></li>
+            <li><a href="listing_1.html">Зергерлік</a></li>
+            <li><a href="listing_1.html">Кітаптар</a></li>
+          </ul>
+      	</div>
+			</div>
+      
+    	<div id="brands_module" class="s_box">
+        <h2>Брендтер</h2>
+        <select onchange="location = this.value">
+          <option> --- Таңдаңыз --- </option>
+          <option>Adidas</option>
+          <option>Apple</option>
+          <option>Armani</option>
+          <option>Balenciaga</option>
+          <option>Bvlgari</option>
+          <option>Canon</option>
+          <option>Christian Dior</option>
+          <option>Elisabeth Arden</option>
+          <option>Hewlett-Packard</option>
+          <option>HTC</option>
+          <option>Hugo Boss</option>
+          <option>Nike</option>
+          <option>Nikon</option>
+          <option>Palm</option>
+          <option>Panasonic</option>
+          <option>Reebok</option>
+          <option>Samsung</option>
+          <option>Sony</option>
+          <option>Swarovski</option>
+        </select>
+      </div>
+      
+      <div id="bestseller_side" class="s_box clearfix">
+        <h2>Бестселлеры</h2>
+        <div class="s_item s_size_1 clearfix">
+          <a class="s_thumb" href="product.php"><img src="images/dummy/pic_10.jpg" width="38" height="38" alt="iPhone 3GS 8GB" /></a>
+          <h3><a href="product.php">iPhone 3GS 8GB</a></h3>
+          <p><a href="product.php"><span class="s_main_color"><span class="s_currency s_before">£</span>587.50</span></a></p>
+        </div>
+        
+        <div class="s_item s_size_1 clearfix">
+          <a class="s_thumb" href="product.php"><img src="images/dummy/pic_9.jpg" width="38" height="38" alt="Hubo Boss Emotion" /></a>
+          <h3><a href="product.php">Hubo Boss Emotion</a></h3>
+          <p><a href="product.php"><span class="s_main_color"><span class="s_currency s_before">£</span>2,350.00</span></a></p>
+        </div>
+        
+        <div class="s_item s_size_1 clearfix">
+          <a class="s_thumb" href="product.php"><img src="images/dummy/pic_8.jpg" width="38" height="38" alt="Panasonic Lumix" /></a>
+          <h3><a href="product.php">Panasonic Lumix</a></h3>
+          <p><a href="product.php"><span class="s_main_color"><span class="s_currency s_before">£</span>94.00</span></a></p>
+        </div>
+        
+        <div class="s_item s_size_1 clearfix">
+          <a class="s_thumb" href="product.php"><img src="images/dummy/pic_7.jpg" width="38" height="38" alt="Casio Exilim Zoom" /></a>
+          <h3><a href="product.php">Casio Exilim Zoom</a></h3>
+          <p><a href="product.php"><span class="s_main_color"><span class="s_currency s_before">£</span>117.50</span></a></p>
+        </div>
+  
+        <div class="s_item s_size_1 clearfix">
+          <a class="s_thumb" href="product.php"><img src="images/dummy/pic_6.jpg" width="38" height="38" alt="Christian Dior Amber Woman" /></a>
+          <h3><a href="product.php">Christian Dior Amber Woman</a></h3>
+          <p><a href="product.php"><span class="s_main_color"><span class="s_currency s_before">£</span>328.99</span></a></p>
+        </div>
+  
+        <div class="s_item s_size_1 clearfix">
+          <a class="s_thumb" href="product.php"><img src="images/dummy/pic_5.jpg" width="38" height="38" alt="iPod Classic" /></a>
+          <h3><a href="product.php">iPod Classic</a></h3>
+          <p><a href="product.php"><span class="s_main_color"><span class="s_currency s_before">£</span>117.50</span></a></p>
+        </div>
+        
+        <div class="s_item s_size_1 clearfix">
+          <a class="s_thumb" href="product.php"><img src="images/dummy/pic_4.jpg" width="38" height="38" alt="Sony VAIO" /></a>
+          <h3><a href="product.php">Sony VAIO</a></h3>
+          <p><a href="product.php"><span class="s_old"><span class="s_currency s_before">£</span>1,175.00</span> <span class="s_secondary_color"><span class="s_currency s_before">£</span>1,057.50</span></a></p>
+          <div class="s_rating_holder clearfix"><p class="s_rating s_rating_small s_rating_5 left"><span style="width: 100%;" class="s_percent"></span></p><span class="left">&nbsp;5/5</span></div>
+        </div>
+  
+        <div class="s_item s_size_1 clearfix">
+          <a class="s_thumb" href="product.php"><img src="images/dummy/pic_3.jpg" width="38" height="38" alt="Canon Digital Ixus 200 IS" /></a>
+          <h3><a href="product.php">Canon Digital Ixus 200 IS</a></h3>
+          <p><a href="product.php"><span class="s_main_color"><span class="s_currency s_before">£</span>117.50</span></a></p>
+        </div>
+    
+        <div class="s_item s_size_1 clearfix">
+          <a class="s_thumb" href="product.php"><img src="images/dummy/pic_2.jpg" width="38" height="38" alt="Hugo Boss Chrome" /></a>
+          <h3><a href="product.php">Hugo Boss Chrome</a></h3>
+          <p><a href="product.php"><span class="s_main_color"><span class="s_currency s_before">£</span>94.00</span></a></p>
+        </div>
+    
+        <div class="s_item s_size_1 clearfix">
+          <a class="s_thumb" href="product.php"><img src="images/dummy/pic_1.jpg" width="38" height="38" alt="Hennesy Paradis Extra" /></a>
+          <h3><a href="product.php">Hennesy Paradis Extra</a></h3>
+          <p><a href="product.php"><span class="s_old"><span class="s_currency s_before">£</span>1,175.00</span> <span class="s_secondary_color"><span class="s_currency s_before">£</span>1,057.50</span></a></p>
+        </div>
+    
+        <div class="s_item s_size_1 clearfix">
+          <a class="s_thumb" href="product.php"><img src="images/dummy/pic_10.jpg" width="38" height="38" alt="Elizabeth Arden Pretty" /></a>
+          <h3><a href="product.php">Elizabeth Arden Pretty</a></h3>
+          <p><a href="product.php"><span class="s_main_color"><span class="s_currency s_before">£</span>94.00</span></a></p>
+        </div>
+    
+        <div class="s_item s_size_1 clearfix">
+          <a class="s_thumb" href="product.php"><img src="images/dummy/pic_9.jpg" width="38" height="38" alt="Sony Cyber Shot S1000" /></a>
+          <h3><a href="product.php">Sony Cyber Shot S1000</a></h3>
+          <p><a href="product.php"><span class="s_main_color"><span class="s_currency s_before">£</span>117.50</span></a></p>
+        </div>
+      </div>
+    
+      <div id="information_module" class="s_box">
+        <h2>Ақпарат</h2>
+        <ul class="s_list_1">
+          <li><a href="static.html">Shoppica туралы</a></li>
+          <li><a href="static.html">Құпиялылық саясат</a></li>
+          <li><a href="static.html">Терминдер &amp; Шарттары</a></li>
+          <li><a href="contacts.html">Бізбен хабарласыңыз</a></li>
+          <li><a href="sitemap.html">Сайт картасы</a></li>
+        </ul>
+      </div>
+  
+      <div id="featured_side" class="s_box clearfix">
+        <h2>Үздік</h2>
+        <div class="s_item s_size_1 clearfix">
+          <a class="s_thumb" href="product.php"><img src="images/dummy/pic_8.jpg" width="38" height="38" alt="White football trainers" /></a>
+          <h3><a href="product.php">White football trainers</a></h3>
+          <p><a href="product.php"><span class="s_main_color"><span class="s_currency s_before">£</span>117.50</span></a></p>
+        </div>
+        <div class="s_item s_size_1 clearfix">
+          <a class="s_thumb" href="product.php"><img src="images/dummy/pic_7.jpg" width="38" height="38" alt="Hubo Boss Emotion" /></a>
+          <h3><a href="product.php">Hubo Boss Emotion</a></h3>
+          <p><a href="product.php"><span class="s_main_color"><span class="s_currency s_before">£</span>2,350.00</span></a></p>
+        </div>
+        <div class="s_item s_size_1 clearfix">
+          <a class="s_thumb" href="product.php"><img src="images/dummy/pic_6.jpg" width="38" height="38" alt="Christian Dior J'adore" /></a>
+          <h3><a href="product.php">Christian Dior J'adore</a></h3>
+          <p><a href="product.php"><span class="s_main_color"><span class="s_currency s_before">£</span>328.99</span></a></p>
+        </div>
+      </div>
+
+    </div>
