359f51279b33ac4199031811d2b830963bf15c45
domakov <domakov91@gmail.com>
2017-05-31 10:34:41 +0500

    индех

--- index.php
+++ index.php
@@ -40,36 +40,64 @@ $sql="SELECT * from tovar";
   <!-- ********************** --> 
   <?var_dump($res);?>
   <div id="intro">
-    <div id="intro_wrap">
-      <div id="product_intro" class="container_12">
-        <div id="product_intro_info" class="grid_5">
-          <div style="position: relative;">
-            <h2><a href="product.php"><?=$res[1]["t_name"];?></a></h2>
-            <div class="s_rating_holder">
-              <p class="s_rating s_rating_big s_rating_5"> <span style="width: 60%;" class="s_percent"></span> </p>
-              <span class="s_average">3 out of 5</span> </div>
-            <p class="s_desc"> баяулауы жоқ үлкен бара артықшылықтарын елестетіп көріңізші. үлкен 19 & дегенді түртіңіз 941BW монитор үлкен суреттер, жұмыс көп бөлмеде және қытырлақ қозғалыс үшін, тез пикселді жауап қайтару уақыт кең арақатынасын біріктіреді. Сонымен қатар, эксклюзивті MagicBright 2, MagicColor және MagicTune технологиялар мың жеткізуге көмектеседі...</p>
-            <div class="s_price_holder">
-              <p class="s_price"> <span class="s_currency s_before">$</span>384.67 </p>
-            </div>
-          </div>
-        </div>
-        <div id="product_intro_preview">
-          <div class="slides_container">
-            <div class="slideItem" style="display: none"><a href="product.php"><img src="images/dummy/pic_1.jpg" alt="" /></a></div>
-            <div class="slideItem" style="display: none"><a href="product.php"><img src="images/dummy/pic_2.jpg" alt=""  /></a></div>
-            <div class="slideItem" style="display: none"><a href="product.php"><img src="images/dummy/pic_3.jpg" alt="" /></a></div>
-            <div class="slideItem" style="display: none"><a href="product.php"><img src="images/dummy/pic_4.jpg" alt="" /></a></div>
-            <div class="slideItem" style="display: none"><a href="product.php"><img src="images/dummy/pic_5.jpg" alt="" /></a></div>
-          </div>
-          <a class="s_button_prev" href="javascript:;"></a>
-          <a class="s_button_next" href="javascript:;"></a>
-        </div>
-      </div>
-    </div>
-  </div>
-	<script type="text/javascript" src="js/jquery/jquery.slides.js"></script> 
-  <script type="text/javascript" src="js/shoppica.products_slide.js"></script>
+ +    <div id="intro_wrap">
+ +      <div id="product_intro" class="container_12">
+ +        <div id="product_intro_info" class="grid_5">
+ +          <div style="position: relative;">
+ +            <h2><a href="product.html">Samsung SyncMaster 941BW</a></h2>
+ +            <div class="s_rating_holder">
+ +              <p class="s_rating s_rating_big s_rating_5"> <span style="width: 60%;" class="s_percent"></span> </p>
+ +              <span class="s_average">3 out of 5</span> </div>
+ +            <p class="s_desc"> Imagine the advantages of going big without slowing down. The big 19&quot; 941BW monitor combines wide aspect ratio with fast pixel response time, for bigger images, more room to work and crisp motion. In addition, the exclusive MagicBright 2, MagicColor and MagicTune technologies help deliver th...</p>
+ +            <div class="s_price_holder">
+ +              <p class="s_price"> <span class="s_currency s_before">$</span>384.67 </p>
+ +            </div>
+ +          </div>
+ +          <div style="position: relative; display: none;">
+ +            <h2><a href="product.html">White football trainers</a></h2>
+ +            <p class="s_desc"> Product 8 ...</p>
+ +            <div class="s_price_holder">
+ +              <p class="s_price"> <span class="s_currency s_before">$</span>192.34 </p>
+ +            </div>
+ +          </div>
+ +          <div style="position: relative; display: none;">
+ +            <h2><a href="product.html">Balenciaga Paris</a></h2>
+ +            <p class="s_desc"> Intel Core 2 Duo processor Powered by an Intel Core 2 Duo processor at speeds up to 2.16GHz, the new MacBook is the fastest ever 1GB memory, larger hard drives The new MacBook now comes with 1GB of memory standard and larger hard drives for the entire line perfect for runni...</p>
+ +            <div class="s_price_holder">
+ +              <p class="s_price"> <span class="s_currency s_before">$</span>961.68 </p>
+ +            </div>
+ +          </div>
+ +          <div style="position: relative; display: none;">
+ +            <h2><a href="product.html">Adidas Acqua</a></h2>
+ +            <p class="s_desc"> Product 8 Morbi malesuada, ante sit amet faucibus pharetra, ipsum ante imperdiet tellus, nec placerat metus sem sed magna. Quisque id urna a metus porttitor tristique et at justo. Praesent non enim in elit sagittis feugiat. Integer in lectus lobortis lacus commodo mattis non auctor metus. Sus...</p>
+ +            <div class="s_price_holder">
+ +              <p class="s_price"> <span class="s_currency s_before">$</span>192.34 </p>
+ +            </div>
+ +          </div>
+ +          <div style="position: relative; display: none;">
+ +            <h2><a href="product.html">Panasonic Lumix</a></h2>
+ +            <p class="s_desc"> Engineered with pro-level features and performance, the 12.3-effective-megapixel D300 combines brand new technologies with advanced features inherited from Nikon&#39;s newly announced D3 professional digital SLR camera to offer serious photographers remarkable performance combined with agilit...</p>
+ +            <div class="s_price_holder">
+ +              <p class="s_price"> <span class="s_currency s_before">$</span>153.87 </p>
+ +            </div>
+ +          </div>
+ +        </div>
+ +        <div id="product_intro_preview">
+ +          <div class="slides_container">
+ +            <div class="slideItem" style="display: none"><a href="product.html"><img src="images/dummy/pic_1.jpg" alt="" /></a></div>
+ +            <div class="slideItem" style="display: none"><a href="product.html"><img src="images/dummy/pic_2.jpg" alt=""  /></a></div>
+ +            <div class="slideItem" style="display: none"><a href="product.html"><img src="images/dummy/pic_3.jpg" alt="" /></a></div>
+ +            <div class="slideItem" style="display: none"><a href="product.html"><img src="images/dummy/pic_4.jpg" alt="" /></a></div>
+ +            <div class="slideItem" style="display: none"><a href="product.html"><img src="images/dummy/pic_5.jpg" alt="" /></a></div>
+ +          </div>
+ +          <a class="s_button_prev" href="javascript:;"></a>
+ +          <a class="s_button_next" href="javascript:;"></a>
+ +        </div>
+ +      </div>
+ +    </div>
+ +  </div>
+ +	<script type="text/javascript" src="js/jquery/jquery.slides.js"></script> 
+ +  <script type="text/javascript" src="js/shoppica.products_slide.js"></script>
   <!-- end of intro --> 
   
   
