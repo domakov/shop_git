d521afc3793b41546ffdf73c6a536b367c6ce82f
klimsalamat <klim.salamat.98@mail.ru>
2017-06-03 12:47:27 +0500

    ссылка категорияларга

--- index.php
+++ index.php
@@ -141,7 +141,7 @@ LIMIT 6
 		  ?>
         <div class="s_item grid_2"> <a class="s_thumb" href="product.php?id=<?=$array["id_tovar"];?>"><img src="<?=$array['f_link'];?>" title="Armani Acqua di Gioia" alt="Armani Acqua di Gioia" /></a>
           <h3><a href="product.php?id=<?=$array["id_tovar"];?>"><?=$array["t_name"];?></a></h3>
-          <p class="s_model"><?=$array["c_name"];?></p>
+          <p class="s_model"><a href="listing_1.php?id=<?=$array["id_category"];?>"><?=$array["c_name"];?></a></p>
           <p class="s_price s_promo_price"><span class="s_old_price"><span class="s_currency s_before">$</span><?=$array['t_price'];?></span><span class="s_currency s_before">$</span>
 		  <?echo ($array["t_price"]-($array["t_price"]*$array["t_sale"]/100))?></p>
           <a class="s_button_add_to_cart" href="product.php"><span class="s_icon_16"><span class="s_icon"></span>Add to Cart</span></a>
@@ -161,7 +161,7 @@ LIMIT 6
 		  foreach($res as $array){?>
 			<div class="s_item grid_2"> <a class="s_thumb" href="product.php?id=<?=$array["id_tovar"];?>"><img src="<?=$array['f_link'];?>" title="Sony Cyber Shot S1000" alt="Sony Cyber Shot S1000" /></a>
 			  <h3><a href="product.php?id=<?=$array["id_tovar"];?>"><?=$array['t_name'];?></a></h3>
-			  <p class="s_model"><?=$array['c_name'];?></p>
+			  <p class="s_model"><a href="listing_1.php?id=<?=$array["id_category"];?>"><?=$array['c_name'];?></a></p>
 			  <p class="s_price"><span class="s_currency s_before">$</span><?=$array['t_price'];?></p>
 			  <a class="s_button_add_to_cart" href="product.php"><span class="s_icon_16"><span class="s_icon"></span>Add to Cart</span></a>
 			</div>
